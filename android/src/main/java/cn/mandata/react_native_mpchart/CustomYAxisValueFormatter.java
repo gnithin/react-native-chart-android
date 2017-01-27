021ea157d119b30370835bd1100d46bbfce933f5
Nithin <nithin.linkin@gmail.com>
2017-01-27 07:01:08 +0530

    [custom-formatter] Add logic for custom formatter

--- android/src/main/java/cn/mandata/react_native_mpchart/CustomYAxisValueFormatter.java
+++ android/src/main/java/cn/mandata/react_native_mpchart/CustomYAxisValueFormatter.java
@@ -7,12 +7,46 @@ import com.github.mikephil.charting.formatter.YAxisValueFormatter;
  * Created by Administrator on 2015/11/7.
  */
 public class CustomYAxisValueFormatter implements YAxisValueFormatter {
-   private String FormAt="";
-   public   CustomYAxisValueFormatter(String formAt){
-       this.FormAt=formAt;
+   private String format="";
+   public CustomYAxisValueFormatter(String format){
+       this.format = format;
    }
     @Override
     public String getFormattedValue(float v, YAxis yAxis) {
-        return String.format(FormAt,v);
+        if(format.equalsIgnoreCase("custom")){
+            return customFormatter(v);
+        } else{
+            return String.format(format, v);
+        }
+    }
+
+    public String customFormatter(float v){
+        String finalStr = "";
+
+        String prefix = "$ ";
+        String suffix = "";
+        String numStr = "";
+
+        if(v < 1000){
+            int numVal = (int)(Math.round(v));
+            numStr = String.format("%d", numVal);
+        } else{
+            String []expSuffixArr = {"K", "M", "B"};
+            int expVal = (int)((Math.floor(Math.log10((double)v)))/3);
+            int expIndex = expVal - 1;
+
+            if (expIndex >= expSuffixArr.length){
+                expIndex = expSuffixArr.length - 1;
+            }
+
+            suffix += expSuffixArr[expIndex];
+
+            int modifiedVal = (int)(Math.round(v/(Math.pow(10, expVal*3))));
+            numStr = String.format("%d", modifiedVal);
+        }
+
+        finalStr = prefix + numStr + suffix;
+
+        return finalStr;
     }
 }
